9b12fe7d09d79be34b675502fd18265f92d5dec9
John Coonrod <john.coonrod@thp.org>
2022-02-21 17:38:32 -0500

    initial setup

---
+++ sw.js
@@ -0,0 +1,48 @@
+cacheName='mcld-v3.5';
+const URLS = [
+  './',
+  "./admin_en.html",
+  "./admin_es.html",
+  "./admin_fr.html",
+  './en.html',
+  "./entext.js",
+  "./fr.html",
+  "./frtext.js",
+  "./fullreport.html",
+  "./es.html",
+  "./estext.js",
+  './functions.js',
+  './index.html',
+  './intro_en.html',
+  './intro_fr.html',
+  './intro_es.html',
+  "./logo150.png",
+  "./logo512.png",
+  "./manifest.json",
+  './style.css',
+  './sw.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(URLS);
+  })());
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+      fetch(e.request).catch(function() {
+          return caches.match(e.request,{'ignoreSearch':true})
+      })
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      caches.delete(key);
+    }))
+  }));
+});
